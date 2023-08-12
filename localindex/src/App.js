37e04a60b8b03c57febb5c36b3d8073d8261a30d
mochi <sumonwin.mm@gmail.com>
2023-08-12 16:32:26 +1200

    basic react

--- localindex/src/App.js
+++ localindex/src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
+import tt from "@tomtom-international/web-sdk-maps"; 
+import Home from './pages/Home';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"> 
+  <Home/>
     </div>
   );
+  // var map = tt.map({
+  //   key: ,
+  //   container: "map",
+  //   dragPan: true
+  // });
+  map.addControl(new tt.FullscreenControl());
+  map.addControl(new tt.NavigationControl());  
 }
+
+
 
 export default App;
